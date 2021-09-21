95872605817f0ac98a2f95caddc37f77f185444e
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-21 13:42:20 -0600

    chore: add interface for coordinate

---
+++ projects/angular-mentions/src/lib/models/coordinate.ts
@@ -0,0 +1,15 @@
+/**
+ * Coordinates for absolute positioning of elements (absolute to top left).
+ */
+export interface Coordinate {
+
+  /** The number of pixels in regards to the absolute top position. */
+  top: number;
+
+  /** The number of pixels in regards to the absolute left position. */
+  left: number;
+
+  /** The height in pixels of ??? */
+  height?: number;
+
+}
