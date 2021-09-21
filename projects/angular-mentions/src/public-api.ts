8f3077dc75dbfac0d2b83afe4433b5f246ee70f1
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-21 13:44:29 -0600

    chore: update public API with new model

--- projects/angular-mentions/src/public-api.ts
+++ projects/angular-mentions/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of angular-mentions
  */
 
-export * from './lib/mention-config';
+export * from './lib/models/mention-config';
 export * from './lib/mention.directive';
 export * from './lib/mention.module';
