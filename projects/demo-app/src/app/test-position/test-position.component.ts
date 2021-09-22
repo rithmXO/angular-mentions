e8b4370ae37859340ca9bc7ce3092b92042e10d1
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-22 17:17:05 -0600

    style: fix lint errors

--- projects/demo-app/src/app/test-position/test-position.component.ts
+++ projects/demo-app/src/app/test-position/test-position.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
-
 import { COMMON_NAMES } from '../common-names';
 
+/**
+ * Component used for testing positions.
+ */
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-test-position',
   templateUrl: './test-position.component.html'
 })
 export class TestPositionComponent {
+  /** A list of common names to use for testing. */
   items: string[] = COMMON_NAMES;
-  scrollValues(e:HTMLElement) {
-    return ` 
-      scrollLeft=${e.scrollLeft}; scrollWidth=${e.scrollWidth}
-      scrollTop=${e.scrollTop}; scrollHeight=${e.scrollHeight}
+
+  /**
+   * Returns a series of scroll values.
+   *
+   * @param element The element to have its scroll values returned.
+   * @returns A series of scroll values.
+   */
+  scrollValues(element: HTMLElement): string {
+    return `
+      scrollLeft=${element.scrollLeft}; scrollWidth=${element.scrollWidth}
+      scrollTop=${element.scrollTop}; scrollHeight=${element.scrollHeight}
     `;
   }
 }
