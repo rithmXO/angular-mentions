6ab4da7cae6d492b8a75192d6f2feab3585aa45e
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-22 16:54:41 -0600

    style: fix lint errors in app component

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -6,23 +6,34 @@ import { COMMON_NAMES } from './common-names';
  * Demo app showing usage of the mentions directive.
  */
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  items: string[] = COMMON_NAMES;
+
+  /** A list of test items (names). */
+  items = COMMON_NAMES;
+
+  /** The path to use for e2e testing. */
   test = this.getPath();
-  private getPath() {
+
+  /**
+   * Gets the path for e2e testing.
+   *
+   * @returns The path to use for testing.
+   */
+  private getPath(): string | null {
     // the path provides direct access to the tests for e2e testing
     switch (window.location.pathname) {
-      case '/config'   : return 'config';
-      case '/events'   : return 'events';
-      case '/async'    : return 'async';
-      case '/options'  : return 'options';
+      case '/config': return 'config';
+      case '/events': return 'events';
+      case '/async': return 'async';
+      case '/options': return 'options';
       case '/templates': return 'templates';
-      case '/pos'      : return 'pos';
-      default          : return null;
+      case '/pos': return 'pos';
+      default: return null;
     }
   }
 }
