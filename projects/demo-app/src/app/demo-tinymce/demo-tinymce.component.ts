2ad6c06f9d479b2b6f58053d3d6e1737e197ebbe
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-22 19:09:23 -0600

    build: fix build error

--- projects/demo-app/src/app/demo-tinymce/demo-tinymce.component.ts
+++ projects/demo-app/src/app/demo-tinymce/demo-tinymce.component.ts
@@ -12,7 +12,7 @@ import { COMMON_NAMES } from '../common-names';
   selector: 'app-demo-tinymce',
   template: `
     <div class="form-group" style="position:relative">
-      <div [mention]="items"></div>
+      <div [mentionItems]="items"></div>
       <div>
         <textarea class="hidden" cols="60" rows="4" id="tmce">{{ htmlContent }}</textarea>
       </div>
