f37c566f8b5a48af06a5c0449c196b555b955e94
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-22 19:32:20 -0600

    refactor: fix build and lint errors; cleanup

--- projects/demo-app/src/app/demo-events/demo-events.component.ts
+++ projects/demo-app/src/app/demo-events/demo-events.component.ts
@@ -1,25 +1,55 @@
 import { Component } from '@angular/core';
 import { COMMON_NAMES } from '../common-names';
 import { MentionConfig } from 'projects/angular-mentions/src/public-api';
+import { MentionItem } from 'projects/angular-mentions/src/lib/models';
 
+/**
+ * Demo component for events.
+ */
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-demo-events',
   templateUrl: './demo-events.component.html'
 })
 export class DemoEventsComponent {
-  mentionConfig:MentionConfig = {returnTrigger:false};
+  /** A list of common names to demo complex items in an object. */
+  complexItems = COMMON_NAMES.map(name => ({ label: name }));
+
+  /** */
+  mentionConfig: MentionConfig = {
+    configs: [
+      {
+        items: this.complexItems,
+        returnTrigger: false
+      }
+    ]
+  };
+
+  /** The output to be displayed. */
   output = '';
-  complexItems: any[] = COMMON_NAMES.map(name => {
-    return {label: name};
-  });
+
   constructor() {
     this.log('Ready...');
   }
-  log(s:string, e?:any) {
-    this.output = new Date().toISOString() + ' ' + 
-      s + (e ? ' ' + JSON.stringify(e) : '') + '\n' + this.output;
+
+  /**
+   * Logs text and an optional event.
+   *
+   * @param text The text to log.
+   * @param event The event to log (optional).
+   */
+  log(text: string, event?: string | MentionItem): void {
+    this.output = new Date().toISOString() + ' ' + text + (event ? ' ' + JSON.stringify(event) : '') + '\n' + this.output;
   }
-  updateConfig(e) {
-    this.mentionConfig = {returnTrigger:e.target.checked};
+
+  /**
+   * Updates the mention config based on the checkbox state.
+   *
+   * @param event The checkbox change event.
+   */
+  updateConfig(event: Event): void {
+    if (event.target) {
+      this.mentionConfig.configs[0].returnTrigger = (event.target as HTMLInputElement).checked;
+    }
   }
 }
