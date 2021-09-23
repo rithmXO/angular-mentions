b258bd63b7618a3e55be7873a7eb1f21d3643874
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-22 19:16:08 -0600

    style: fix lint errors

--- projects/demo-app/src/app/demo-options/demo-options.component.ts
+++ projects/demo-app/src/app/demo-options/demo-options.component.ts
@@ -1,30 +1,42 @@
 import { Component } from '@angular/core';
 
+/**
+ * Demo component for showing complex config items.
+ */
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-demo-options',
   templateUrl: './demo-options.component.html'
 })
 export class DemoOptionsComponent {
 
-  format(item:any) {
+  /**
+   * Formats an item to all uppercase.
+   *
+   * @param item The item to format.
+   * @returns A formatted string.
+   */
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  format(item: any): string {
     return item['value'].toUpperCase();
   }
 
+  /** Example list of complex items. */
   complexItems = [
     {
-      "value" : "user1",
-      "email": "user1@domain.com",
-      "name": "User One"
+      value: 'user1',
+      email: 'user1@domain.com',
+      name: 'User One'
     },
     {
-      "value" : "user2",
-      "email": "user2@domain.com",
-      "name": "User Two"
+      value: 'user2',
+      email: 'user2@domain.com',
+      name: 'User Two'
     },
     {
-      "value" : "user3",
-      "email": "user3@domain.com",
-      "name": "User Three"
+      value: 'user3',
+      email: 'user3@domain.com',
+      name: 'User Three'
     }
   ];
 }
