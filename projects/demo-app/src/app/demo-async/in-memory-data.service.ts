c63ca10472c527c3d797ca94b235b64a7e9ece16
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-22 19:55:02 -0600

    style: fix lint issues

--- projects/demo-app/src/app/demo-async/in-memory-data.service.ts
+++ projects/demo-app/src/app/demo-async/in-memory-data.service.ts
@@ -1,9 +1,30 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { COMMON_NAMES } from '../common-names';
 
+/**
+ * Creates a mock database to return items on HTTP request.
+ */
 export class InMemoryDataService implements InMemoryDbService {
-  createDb() {
-    let objects = COMMON_NAMES.map(function(name){return {'label':name}});
-    return {names:COMMON_NAMES,objects:objects};
+  /**
+   * Creates the mock database to return items on HTTP request.
+   *
+   * @returns Mock data from common names.
+   */
+  createDb(): MockHTTPData {
+    const objects = COMMON_NAMES.map(function(name) {
+      return { label: name };
+    });
+    return { names: COMMON_NAMES, objects: objects };
   }
+}
+
+interface MockHTTPData {
+  /** Names. */
+  names: string[];
+
+  /** Objects with name. */
+  objects: {
+    /** Label/name for the object. */
+    label: string;
+  }[];
 }
