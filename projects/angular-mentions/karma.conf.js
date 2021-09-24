ae05b425c8a77957c20a17dc55b0e20c8291f730
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-24 11:19:51 -0600

    test: update Karma configs with browserless Chrome support

--- projects/angular-mentions/karma.conf.js
+++ projects/angular-mentions/karma.conf.js
@@ -26,6 +26,15 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    browserConsoleLogOptions: {
+      level: 'log'
+    },
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
